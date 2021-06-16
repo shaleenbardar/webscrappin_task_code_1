577eeb4035a0e19d090bffad21f9c8642b690c50
Shaleen Bardar <72783766+shaleenbardar@users.noreply.github.com>
2021-06-16 11:38:18 +0530

    Add files via upload

---
+++ scrapping/src/main/java/com/shaleen/scrapping/scrapping/App.java
@@ -0,0 +1,41 @@
+package com.shaleen.scrapping.scrapping;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+class myscrapy
+{
+	public static void main( String[] args ) throws IOException
+	{    
+    //fetching web page via HTTP	
+    Document page = Jsoup.connect("https://prefeitura.pbh.gov.br/saude/licitacao/pregao-eletronico-151-2020").get();
+    	
+    //selecting all hyperlinks
+    Elements pageElements = page.select("a[href]"); //This will fetch all the anchor tags that are there in webpage
+    //Creating an array
+    ArrayList<String> hyperLinks = new ArrayList<String>();
+    //Selecting the Publication date 
+    Element publicationDate = page.selectFirst("#block-views-block-view-noticia-pbh-block-5 > div > div > div > div > div > div.views-field.views-field-nothing > span > span:nth-child(1)");
+    System.out.println(publicationDate.text());
+    //Selecting Bidding date
+    Element biddingDate = page.selectFirst("#block-views-block-view-noticia-pbh-block-5 > div > div > div > div > div > div.views-field.views-field-nothing > span > span:nth-child(19)");
+    System.out.println(biddingDate.text());
+    //selecting the Object value
+    Element object = page.selectFirst("#block-views-block-view-noticia-pbh-block-5 > div > div > div > div > div > div.views-field.views-field-nothing > span > p:nth-child(6)");
+    System.out.println(object.text());
+    //iterating and extracting
+    //Printing the links along with the text associated with it
+    for (Element e:pageElements) {
+      hyperLinks.add("Text: " + e.text());
+      hyperLinks.add("Link: " + e.attr("href"));
+    }
+    //Printing the hyperlinks
+    for (String s : hyperLinks) {
+      System.out.println(s);
+    }
+  }
+}
